4461bac299d8338caad16ed88c7709bc8a9ac9d6
Ayoub Gharbi <contact@ayoub-gharbi.org>
2019-11-15 07:14:51 +0100

    Init Editor History

---
+++ Assets/EditorHistory/Editor/EditorHistoryWindow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class EditorHistoryWindow : EditorWindow
+{
+    private static EditorHistoryWindow _hWindow = null;
+
+    [MenuItem ("Window/GhAyoub/Editor History _F1")]
+    static void OpenhWindow ()
+    {
+        _hWindow = (EditorHistoryWindow) EditorWindow.GetWindow (typeof (EditorHistoryWindow),
+            utility : false, "Editor History", focus : true);
+    }
+
+    void OnGUI ()
+    {
+
+    }
+}
---
+++ Assets/EditorHistory/EditorHistory.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditorHistory
+{
+    private int _selectedHistoryIndex = -1;
+    private object _selectedHistoryObject = null;
+    private List<object> _historyElements = new List<object> ();
+
+    public int HistorySize => _historyElements.Count;
+    public bool IsElementSelected (object hElement) => _selectedHistoryObject == hElement ||
+        _historyElements.FindIndex (h => h == hElement) == _selectedHistoryIndex;
+
+    public List<object> HistoryElements => _historyElements;
+
+    public void AddhEelement (object hElement)
+    {
+        if (!_historyElements.Contains (hElement))
+        {
+            _historyElements.Add (hElement);
+        }
+        else
+        {
+            Debug.LogFormat ("{0} exists already!", hElement.GetType ().Name);
+        }
+    }
+
+    public void RemovehEelement (object hElement)
+    {
+        if (!_historyElements.Contains (hElement))
+        {
+            Debug.LogFormat ("can't remove {0}..", hElement.GetType ().Name);
+            return;
+        }
+
+        var hIndex = _historyElements.FindIndex(h => h == hElement);
+        _historyElements.RemoveAt(hIndex);
+    }
+}
