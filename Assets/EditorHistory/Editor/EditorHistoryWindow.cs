bab9859bfa34b411bc6bce851500bb91a87f1381
Ayoub Gharbi <contact@ayoub-gharbi.org>
2019-11-15 10:04:51 +0100

    Detect and add selected objects in the scene to the history window

--- Assets/EditorHistory/Editor/EditorHistoryWindow.cs
+++ Assets/EditorHistory/Editor/EditorHistoryWindow.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
@@ -14,8 +15,58 @@ public class EditorHistoryWindow : EditorWindow
             utility : false, "Editor History", focus : true);
     }
 
+    public GUISkin _hWindowSkin;
+
+    private bool _isInitialized = false;
+    private EditorHistory _editorHistory = new EditorHistory ();
+
     void OnGUI ()
     {
+        if (!_isInitialized)
+        {
+            Selection.selectionChanged += OnSelectionChanged;
+            _isInitialized = true;
+        }
 
+        EditorGUILayout.BeginHorizontal ();
+
+        GUILayout.Space (15);
+
+        var rect = EditorGUILayout.BeginVertical ();
+
+        DrawHistory ();
+
+        EditorGUILayout.EndVertical ();
+
+        GUILayout.Space (15);
+
+        EditorGUILayout.EndHorizontal ();
+
+    }
+
+    private void OnSelectionChanged ()
+    {
+        var activeObject = Selection.activeGameObject;
+        if (activeObject == null) return;
+
+        DrawHElement (activeObject);
+    }
+
+    void DrawHistory ()
+    {
+        for (int h = 0; h < _editorHistory.HistorySize; h++)
+        {
+            Debug.Log ("Draw!");
+            var hObject = _editorHistory.HistoryElements[h];
+            var buttonStyle = _hWindowSkin.GetStyle ("InteractableButton");
+            GUILayout.Label (hObject.name, buttonStyle);
+
+            GUILayout.Space (15);
+        }
+    }
+
+    void DrawHElement (UnityEngine.Object hObject)
+    {
+        _editorHistory.AddhEelement (hObject);
     }
 }
--- Assets/EditorHistory/EditorHistory.cs
+++ Assets/EditorHistory/EditorHistory.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class EditorHistory
 {
     private int _selectedHistoryIndex = -1;
-    private object _selectedHistoryObject = null;
-    private List<object> _historyElements = new List<object> ();
+    private Object _selectedHistoryObject = null;
+    private List<Object> _historyElements = new List<Object> ();
 
     public int HistorySize => _historyElements.Count;
-    public bool IsElementSelected (object hElement) => _selectedHistoryObject == hElement ||
+    public bool IsElementSelected (Object hElement) => _selectedHistoryObject == hElement ||
         _historyElements.FindIndex (h => h == hElement) == _selectedHistoryIndex;
 
-    public List<object> HistoryElements => _historyElements;
+    public List<Object> HistoryElements => _historyElements;
 
-    public void AddhEelement (object hElement)
+    public void AddhEelement (Object hElement)
     {
         if (!_historyElements.Contains (hElement))
         {
@@ -22,15 +22,15 @@ public class EditorHistory
         }
         else
         {
-            Debug.LogFormat ("{0} exists already!", hElement.GetType ().Name);
+            Debug.LogFormat ("{0} exists already!", hElement.name);
         }
     }
 
-    public void RemovehEelement (object hElement)
+    public void RemovehEelement (Object hElement)
     {
         if (!_historyElements.Contains (hElement))
         {
-            Debug.LogFormat ("can't remove {0}..", hElement.GetType ().Name);
+            Debug.LogFormat ("can't remove {0}..", hElement.name);
             return;
         }
 
